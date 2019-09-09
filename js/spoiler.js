03e27841178ed89fc3355ba5c5f3ba4a06111835
tiger0132 <4c5948@gmail.com>
2019-09-09 21:19:04 +0800

    Site updated: 2019-09-09 21:19:04

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,10 +1,8 @@
-function qwq() {
-    [].forEach.call(document.getElementsByClassName('spoiler'), function (panel) {
-        panel.getElementsByClassName('spoiler-title')[0].onclick = function () {
+(function (document) {
+    [].forEach.call(document.getElementsByClassName('spoiler'), function(panel) {
+        panel.getElementsByClassName('spoiler-title')[0].onclick = function() {
             panel.classList.toggle("collapsed");
             panel.classList.toggle("expanded");
         }
     });
-}
-qwq();
-document.addEventListener('pjax:complete', qwq);
+})(document);
